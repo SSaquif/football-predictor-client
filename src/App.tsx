049e474cebc61e80dc23bddc32d1071ebd5bfdd8
Sadnan Saquif <ssaquif@gmail.com>
2022-08-09 00:00:00 -0400

    Holy Grail Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,50 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  /* border: 1rem solid cornflowerblue; */
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+const Header = styled.header`
+  border: 1px solid yellow;
+  height: 80px;
+`;
+const MainContentContainer = styled.div`
+  border: 1px solid greenyellow;
+  flex: 1;
+  display: flex;
+`;
+
+const LeftSidebar = styled.aside`
+  border: 1px solid lime;
+  flex: 1;
+`;
+const Main = styled.main`
+  border: 1px solid blue;
+  flex: 3;
+`;
+const RightSidebar = styled.aside`
+  border: 1px solid hotpink;
+  flex: 1;
+`;
+const Footer = styled.footer`
+  border: 1px solid red;
+  height: 150px;
+`;
+
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <Container>
+      <Header>Header</Header>
+      <MainContentContainer>
+        <LeftSidebar>Left Sidebar</LeftSidebar>
+        <Main>Main</Main>
+        <RightSidebar>Right Sidebar</RightSidebar>
+      </MainContentContainer>
+      <Footer>Footer</Footer>
+    </Container>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import PredictionTable from "./components/PredictionTable";
-import { EPL_TEAMS_2022_2023 } from "./data";
+import GlobalStyle from "./styles/GlobalStyles";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <PredictionTable isEditing teams={EPL_TEAMS_2022_2023} />
+    <GlobalStyle />
+    <App />
   </React.StrictMode>
 );
